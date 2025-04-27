77616a925e3b665581870d9bbf45efd2dbcccebc
WallBreakerNO4 <zhifanguo@gmail.com>
2025-04-27 08:43:24 +0000

    取消记录API的日志

--- libs/config.py
+++ libs/config.py
@@ -13,8 +13,8 @@ def load_config() -> Config:
     telegram_token = os.getenv("TELEGRAM_BOT_TOKEN")
     tarkov_api_key = os.getenv("TARKOV_MARKET_API_TOKEN")
     
-    print(f"Loaded Telegram Token: {'*' * len(telegram_token) if telegram_token else 'None'}")
-    print(f"Loaded Tarkov API Key: {'*' * len(tarkov_api_key) if tarkov_api_key else 'None'}")
+    # print(f"Loaded Telegram Token: {'*' * len(telegram_token) if telegram_token else 'None'}")
+    # print(f"Loaded Tarkov API Key: {'*' * len(tarkov_api_key) if tarkov_api_key else 'None'}")
     
     if not telegram_token or not tarkov_api_key:
         raise ValueError("Missing required environment variables")
