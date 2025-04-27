5582c8ca2758cf231900adbf53c681c20d8a636a
WallBreakerNO4 <zhifanguo@gmail.com>
2025-04-27 08:11:15 +0000

    添加群聊命令查询功能

--- handlers.py
+++ handlers.py
@@ -10,11 +10,22 @@ def setup_handlers(application):
     async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             "欢迎使用Tarkov Market查询机器人！\n"
-            "请输入物品名称查询价格信息"
+            "请输入物品名称查询价格信息\n"
+            "在群组中使用/price [物品名]查询"
         )
     
-    async def search_item(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        item_name = update.message.text
+    async def price_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if not context.args:
+            await update.message.reply_text("请指定物品名称，例如: /price 比特币")
+            return
+            
+        item_name = " ".join(context.args)
+        await search_item(update, context, item_name)
+    
+    async def search_item(update: Update, context: ContextTypes.DEFAULT_TYPE, item_name=None):
+        if item_name is None:
+            item_name = update.message.text
+            
         items = api.search_item(item_name)
         
         if not items:
@@ -32,4 +43,5 @@ def setup_handlers(application):
         await update.message.reply_text(message)
     
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("price", price_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, search_item))
